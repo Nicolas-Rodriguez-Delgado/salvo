110fd8560fd10b590512c26a68ef021103e379d9
Nicolas-Rodriguez-Delgado <spastikcitizen@gmail.com>
2018-11-07 17:34:49 +0100

    Basic configuration of Application security enabling URL access to authorized users

--- src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
+++ src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
      Player findByUserName (String userName);
+     Player findByEmail (String email);
 
 }
--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -4,27 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
-import javax.naming.AuthenticationException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.List;
 
@@ -159,24 +153,29 @@ public class SalvoApplication {
 @Configuration
 class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
-	@Service
-	public class MyUserDetailsService implements UserDetailsService {
+	@Autowired
+	private PlayerRepository playerRepository;
 
-		@Autowired
-		private PlayerRepository playerRepository;
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(inputName-> {
 
-		@Override
-		public UserDetails loadUserByUsername(String username) {
-			Player player = playerRepository.findByUserName(username);
-			if (player == null) {
-				throw new UsernameNotFoundException(username);
-			}
-			return User.withDefaultPasswordEncoder()
-					.username(username)
+//			System.out.println(inputName);
+			Player player = playerRepository.findByEmail(inputName);
+//			System.out.println(player);
+
+			if (player != null) {
+//				System.out.println(player);
+				return User.withDefaultPasswordEncoder()
+					.username(player.getEmail())
 					.password(player.getPassword())
-					.roles("")
+					.roles("USER")
 					.build();
-		}
+
+			} else {
+				throw new UsernameNotFoundException("Unknown user: " + inputName);
+			}
+		});
 	}
 }
 
@@ -188,20 +187,43 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/rest/**").denyAll()
-				.antMatchers("/api/games").hasAnyRole("user");
+				.antMatchers("/**").permitAll();
+//				.antMatchers("/rest/**").denyAll()
+//				.antMatchers("/api/games").hasAnyRole("USER");
 
 		http.formLogin()
-				.usernameParameter("user").passwordParameter("password").loginPage("/api/login");
+				.usernameParameter("username").passwordParameter("password").loginPage("/api/login");
 
 		http.logout().logoutUrl("/api/logout");
 
+		// turn off checking for CSRF tokens
 		http.csrf().disable();
+
+		// if user is not authenticated, just send an authentication failure response
+		http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+		// if login is successful, just clear the flags asking for authentication
+		http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+		// if login fails, just send an authentication failure response
+		http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+		// if logout is successful, just send a success response
+		http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+	}
+
+	private void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		}
+	}
+}
 //		http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
 //		http.formLogin().successHandler(authenticationSuccessHandler);
 //		http.formLogin().failureHandler(authenticationFailureHandler);
-	}
-}
+
+
 
 //@Component
 //public class RESTAuthenticationEntryPoint implements AuthenticationEntryPoint {
